208e71d38646388e30886ebaab2a988813760098
Annista <stephanie11foster@gmail.com>
2021-05-22 20:14:23 -0500

    Modified script.js: Created div element allGridTilesDiv and gave it the id 'all-grid-tiles-div'. All of the 16X16 grid tiles were put into this div, and after, allGridTilesDiv was appended to mainGridDiv

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ body.style.backgroundColor= "pink";
 const mainGridDiv= document.createElement("div");
 mainGridDiv.setAttribute("id", "main-grid-div");
 
+const allGridTilesDiv= document.createElement("div");
+allGridTilesDiv.setAttribute("id", "all-grid-tiles-div");
+
 mainGridDiv.style.backgroundColor= "purple";
-mainGridDiv.style.padding= "30px";
+//mainGridDiv.style.padding= "30px";
 
 for(let i=0; i<(16*16); i++){
     const singleGridDiv= document.createElement("div");
@@ -13,11 +16,11 @@ for(let i=0; i<(16*16); i++){
     //singleGridDiv.style.height= "10px";
     //singleGridDiv.style.width= "10px";
     //singleGridDiv.style.margin= "10px";
-    singleGridDiv.classList.add("single-grid-tile");
-    mainGridDiv.appendChild(singleGridDiv);
+    singleGridDiv.classList.add("all-grid-tiles");
+    allGridTilesDiv.appendChild(singleGridDiv);
 }
 
-
+mainGridDiv.appendChild(allGridTilesDiv);
 
 body.appendChild(mainGridDiv);
 
