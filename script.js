d5f0a4942d82e1b484c87ec720562122e51957f6
Annista <stephanie11foster@gmail.com>
2021-05-26 15:41:25 -0500

    Modified script.js: Set the grid container (allGridTilesDiv) width and height to 500px; Created variable squaresEachSide and included in in the for loop so that it would iterate a total of squaresEachSide*squaresEachSide times; Set the width and height of each grid square (singleGridDiv) to 100%; Set both the number of columns and rows to be squaresEachSide and each total length to be divided evenly among the number of rows and columns

--- script.js
+++ script.js
@@ -15,18 +15,33 @@ mainGridDiv.style.backgroundColor= "purple";
 
 const allGridTilesDiv= document.createElement("div");  //div that contains the grid squares
 allGridTilesDiv.setAttribute("id", "all-grid-tiles-div");
+allGridTilesDiv.style.width= "500px";
+allGridTilesDiv.style.height= "500px";
+let squaresEachSide=100; //Number of grid squares/tiles on each side of the grid
 
 
-
-for(let i=0; i<(16*16); i++){          //This for loop creates a total of 256 div, which will make up our grid
+for(let i=0; i<(squaresEachSide*squaresEachSide); i++){          //This for loop creates a total of 256 div, which will make up our grid
     const singleGridDiv= document.createElement("div");
     singleGridDiv.classList.add("all-grid-tiles");
 
     singleGridDiv.addEventListener("mouseover", ()=>{
         singleGridDiv.style.backgroundColor= "black"; //when the mouse hovers over each grid tile, it will
     });                                               //turn black
+
+    singleGridDiv.style.height= "100%";//The height and width of each div is set to 100% of the grid square. 
+    singleGridDiv.style.width= "100%";//The exact measurement will depend on the number of squares in the grid
+
+
+
     allGridTilesDiv.appendChild(singleGridDiv);
 }
+
+allGridTilesDiv.style.gridTemplateColumns= `repeat(${squaresEachSide}, 1fr)`;
+allGridTilesDiv.style.gridTemplateRows= `repeat(${squaresEachSide}, 1fr)`;
+//To get the track size of each column (or row) the total column length is divided evenly by 
+//the number of columns the grid is supposed to have(squaresEachSide) 
+//I think that's what 1fr means :/
+
 
 mainGridDiv.appendChild(allGridTilesDiv);
 
@@ -41,7 +56,4 @@ clearButton.addEventListener("click", ()=>{
 });
 
 body.appendChild(clearButton);
-
-
-
 
