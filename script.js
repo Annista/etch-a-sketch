df0826f28788046a596ca2fb794168f526affbb0
Annista <stephanie11foster@gmail.com>
2021-05-28 00:51:25 -0500

    Modified script.js: Added function createGrid(), which is where a grid would be made with a number of tiles determined by the user; the color of the tile when the mouse hovers over it was changed from black to a randomized color; Added function propmtGrid(), which would ask the user for the number of squares they want the grid to have on each side; Added function changeGrid(), which once again, asks the user for the number of grid tiles and changes the grid (without reloading the window); Added function startEtchASketch(), which adds all the elements to the body; In the clearButton event listener, function changeGrid was called 2 seconds after all grid tiles where cleared

--- script.js
+++ script.js
@@ -17,43 +17,73 @@ const allGridTilesDiv= document.createElement("div");  //div that contains the g
 allGridTilesDiv.setAttribute("id", "all-grid-tiles-div");
 allGridTilesDiv.style.width= "500px";
 allGridTilesDiv.style.height= "500px";
-let squaresEachSide=100; //Number of grid squares/tiles on each side of the grid
 
 
-for(let i=0; i<(squaresEachSide*squaresEachSide); i++){          //This for loop creates a total of 256 div, which will make up our grid
-    const singleGridDiv= document.createElement("div");
-    singleGridDiv.classList.add("all-grid-tiles");
+function createGrid(squaresEachSide){//squaresEachSide-Number of grid squares/tiles on each side of the grid
 
-    singleGridDiv.addEventListener("mouseover", ()=>{
-        singleGridDiv.style.backgroundColor= "black"; //when the mouse hovers over each grid tile, it will
-    });                                               //turn black
+    for(let i=0; i<(squaresEachSide*squaresEachSide); i++){          //This for loop creates a total of 256 div, which will make up our grid
+        const singleGridDiv= document.createElement("div");
+        singleGridDiv.classList.add("all-grid-tiles");
 
-    singleGridDiv.style.height= "100%";//The height and width of each div is set to 100% of the grid square. 
-    singleGridDiv.style.width= "100%";//The exact measurement will depend on the number of squares in the grid
+        singleGridDiv.addEventListener("mouseover", ()=>{
+
+            const randColor= `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
+            singleGridDiv.style.backgroundColor=randColor;
+            //singleGridDiv.style.backgroundColor= "black"; //when the mouse hovers over each grid tile, it will
+        });                                               //turn black
+
+        singleGridDiv.style.height= "100%";//The height and width of each div is set to 100% of the grid square. 
+        singleGridDiv.style.width= "100%";//The exact measurement will depend on the number of squares in the grid
 
 
 
-    allGridTilesDiv.appendChild(singleGridDiv);
+        allGridTilesDiv.appendChild(singleGridDiv);
+    }
+    allGridTilesDiv.style.gridTemplateColumns= `repeat(${squaresEachSide}, 1fr)`;
+    allGridTilesDiv.style.gridTemplateRows= `repeat(${squaresEachSide}, 1fr)`;
+    //To get the track size of each column (or row) the total column length is divided evenly by 
+    //the number of columns the grid is supposed to have(squaresEachSide) 
+    //I think that's what 1fr means :/
+
 }
 
-allGridTilesDiv.style.gridTemplateColumns= `repeat(${squaresEachSide}, 1fr)`;
-allGridTilesDiv.style.gridTemplateRows= `repeat(${squaresEachSide}, 1fr)`;
-//To get the track size of each column (or row) the total column length is divided evenly by 
-//the number of columns the grid is supposed to have(squaresEachSide) 
-//I think that's what 1fr means :/
+function promptGrid(){
+    let numberSquares= prompt("How many squares do  you want on each side of the grid?");
+    return numberSquares;
+}
+
+function changeGrid(){
 
 
-mainGridDiv.appendChild(allGridTilesDiv);
+    while(allGridTilesDiv.firstChild){
+        allGridTilesDiv.removeChild(allGridTilesDiv.lastChild);
+    }
 
-body.appendChild(mainGridDiv);
+    mainGridDiv.removeChild(allGridTilesDiv);
+    let noSquares= promptGrid();
+    createGrid(noSquares);
+    mainGridDiv.appendChild(allGridTilesDiv);
+}
 
 
+function startEtchASketch(){
+
+    createGrid(promptGrid());
+
+    mainGridDiv.appendChild(allGridTilesDiv);
+
+    body.appendChild(clearButton);
+
+    body.appendChild(mainGridDiv);
+}
 
 clearButton.addEventListener("click", ()=>{
     document.querySelectorAll(".all-grid-tiles").forEach((tile)=>{
-        tile.style.backgroundColor= "white"; //clears all grid tiles by making them white again
+        tile.style.backgroundColor= "white"; //clears all grid tiles by making them white again    
     });
+
+    setTimeout(changeGrid, 2000);    
 });
 
-body.appendChild(clearButton);
+startEtchASketch();
 
